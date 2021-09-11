262f79916e10fba0c484a5061f3349eb67321c63
Jyeverson Madalozo <jyeversonmadalozodev@gmail.com>
2021-09-10 21:32:33 -0300

    Real time clock

--- app/src/main/java/com/example/theclock/ClockActivity.kt
+++ app/src/main/java/com/example/theclock/ClockActivity.kt
@@ -1,17 +1,11 @@
 package com.example.theclock
 
 import android.content.pm.ActivityInfo
-import android.opengl.ETC1.getWidth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.os.Handler
-import android.util.TypedValue
-import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 import java.util.*
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
 
 class ClockActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,24 +13,25 @@ class ClockActivity : AppCompatActivity() {
         setContentView(R.layout.activity_clock)
 
         this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE
-        timer()
-    }
 
-    fun timer(){
-        val textTiming = findViewById<TextView>(R.id.txt_clock)
-        val calendar = Calendar.getInstance()
-        calendar.timeInMillis = System.currentTimeMillis()
-        val updateTimer = String.format("%02d:%02d", calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE))
-
-        val countDownTimer = object : CountDownTimer(60000, 1){
-            override fun onTick(millisUntilFinished: Long) {
-                textTiming.setText(updateTimer)
-            }
-            override fun onFinish() {
+        val t: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    while (!isInterrupted) {
+                        sleep(1000)
+                        runOnUiThread {
+                            val textTiming = findViewById<TextView>(R.id.txt_clock)
+                            val calendar = Calendar.getInstance()
+                            calendar.timeInMillis = System.currentTimeMillis()
+                            val updateTimer = String.format("%02d:%02d", calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE))
+                            textTiming.setText(updateTimer)
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                }
             }
         }
-        countDownTimer.start()
+        t.start()
 
     }
-
 }
