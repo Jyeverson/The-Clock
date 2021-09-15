4e2c1ea526b9952b153d43dbb9ab3588c827fc05
Jyeverson Madalozo <jyeversonmadalozodev@gmail.com>
2021-09-15 00:21:22 -0300

    Upgrade StopWatch xml

--- app/src/main/java/com/example/theclock/stopwatch/StopwatchActivity.kt
+++ app/src/main/java/com/example/theclock/stopwatch/StopwatchActivity.kt
@@ -1,12 +1,23 @@
 package com.example.theclock.stopwatch
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.example.theclock.R
 
 class StopwatchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        
         setContentView(R.layout.activity_stopwatch)
+
+        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE
     }
 }
