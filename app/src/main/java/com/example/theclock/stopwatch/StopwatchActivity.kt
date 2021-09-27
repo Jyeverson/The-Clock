b6735220612e2ccfced57fe153183a74454aa83a
Jyeverson-Madalozo <jyeversonsantos@gmail.com>
2021-09-27 01:20:58 -0300

    Stopwatch progress

--- app/src/main/java/com/example/theclock/stopwatch/StopwatchActivity.kt
+++ app/src/main/java/com/example/theclock/stopwatch/StopwatchActivity.kt
@@ -1,19 +1,24 @@
 
 package com.example.theclock.stopwatch
 
-import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.Window
 import android.view.WindowManager
-import android.widget.ProgressBar
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.preference.PreferenceManager
 import com.example.theclock.R
+import android.view.View
+import java.util.*
+
 
 class StopwatchActivity : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,20 +29,21 @@ class StopwatchActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_stopwatch)
 
-
-        titleActionBar()
+        progress()
     }
-
-    private fun titleActionBar() {
+    private fun progress() {
         val seekBar = findViewById<SeekBar>(R.id.progressBar)
         seekBar.max = (durationSeekBar()?.times(60000))?.toInt()!!
         val countDownTimer =
             object : CountDownTimer(durationSeekBar()?.times(60000)?.toLong()!!, 1) {
                 override fun onTick(millisUntilFinished: Long) {
                         val minute = (millisUntilFinished / 1000) / 60
-                        val seconds = (millisUntilFinished / 1000) % 60
+                        var seconds = (millisUntilFinished / 1000) % 60
                     seekBar.progress =
                             (durationSeekBar()?.times(60000)?.minus(millisUntilFinished))?.toInt()!!
+                    val tv = findViewById<View>(R.id.txt_stopwatch) as TextView
+                    tv.text = "$minute:$seconds"
+                    seconds -= 1
 
                 }
 
@@ -46,7 +52,6 @@ class StopwatchActivity : AppCompatActivity() {
             }
         countDownTimer.start()
     }
-
 
     fun durationSeekBar(): Double? {
         val sharedPref = PreferenceManager.getDefaultSharedPreferences(applicationContext)
